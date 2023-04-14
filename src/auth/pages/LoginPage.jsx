fc0550bfe3f31ff4e23da6cdcbcbf2780889e3f7
Daniela Carballo <danscarballo@gmail.com>
2023-04-14 13:34:59 -0300

    Dispara accion de autenticacion

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
-import { useDispatch } from "react-redux";
+import { useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link as RouterLink } from "react-router-dom";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
 import { Google } from "@mui/icons-material";
@@ -8,11 +9,15 @@ import { checkingAuthentication, startGoogleSignIn } from "../../store/auth";
 
 export const LoginPage = () => {
 
+  const { status } = useSelector((state) => state.auth);
+
   const dispatch = useDispatch();
   const { email, password, onInputChange } = useForm({
     email: 'Daniela@google.com',
     password: '123456'
   });
+
+  const isAuthenticating = useMemo( () => status === 'checking', [status] );
 
   const onSubmit = ( event ) => {
     event.preventDefault();
@@ -57,7 +62,12 @@ export const LoginPage = () => {
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12} sm={6}>
-              <Button type="submit" variant="contained" fullWidth>
+              <Button
+                type="submit"
+                variant="contained"
+                fullWidt
+                disabled={ isAuthenticating }
+              >
                 Login
               </Button>
             </Grid>
@@ -66,6 +76,7 @@ export const LoginPage = () => {
                 variant="contained"
                 fullWidth
                 onClick={ onGoogleSignIn }
+                disabled={ isAuthenticating }
               >
                 <Google />
                 <Typography sx={{ ml: 1 }}>Google</Typography>
