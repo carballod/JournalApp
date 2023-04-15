fb09c5aaa16a06949f0b32ca8a5409976dea248a
Daniela Carballo <danscarballo@gmail.com>
2023-04-15 19:37:44 -0300

    Checking Authentication

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,17 @@
 import { Route, Routes } from 'react-router-dom'
 import { AuthRoutes } from '../auth/routes/AuthRoutes'
 import { JournalRoute } from '../journal/routes/JournalRoute'
+import { useSelector } from 'react-redux'
+import { CheckingAuth } from '../ui'
 
 export const AppRouter = () => {
+
+  const { status } = useSelector( state => state.auth );
+
+  if ( status === 'checking' ) {
+    return <CheckingAuth />
+  }
+
   return (
     <Routes>
 
