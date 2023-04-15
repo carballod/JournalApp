c5440beb82f0a6461fbc20cd67ac3ebb010dc98a
Daniela Carballo <danscarballo@gmail.com>
2023-04-15 16:13:05 -0300

    Login de usuario con correo y contraseña

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,4 +1,4 @@
-import { registerUserWithEmailPassword, singInWithGoogle } from "../../firebase/providers";
+import { loginWithEmailPassword, registerUserWithEmailPassword, singInWithGoogle } from "../../firebase/providers";
 import { checkingCredentials, logout, login } from "./";
 
 export const checkingAuthentication = ( email, password)  => {
@@ -29,11 +29,25 @@ export const startCreatingUserWithEmailPassword = ({ email, password, displayNam
 
     dispatch( checkingCredentials() );
 
-    const { ok, uid, photoURL, errorMessage } = await registerUserWithEmailPassword({ email, password, displayName });
+    const result = await registerUserWithEmailPassword({ email, password, displayName });
 
-    if ( !ok ) return dispatch ( logout({ errorMessage }) );
-
-    dispatch( login({ uid, displayName, email, photoURL }) );
+    if ( !result.ok ) return dispatch ( logout( result.errorMessage ) );
+    dispatch( login( result ) );
 
   };
 }
+
+export const startLoginWithEmailPassword = ({ email, password }) => {
+  return async ( dispatch ) => {
+
+    dispatch( checkingCredentials() );
+
+    const result = await loginWithEmailPassword({ email, password });
+
+    console.log(result);
+
+    if ( !result.ok ) return dispatch ( logout( result ) );
+    dispatch( login( result ) );
+
+  };
+};
