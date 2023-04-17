a03c06e3ecbecd2717b0047b05eb0cdd6b4e1399
Daniela Carballo <danscarballo@gmail.com>
2023-04-16 23:26:38 -0300

    Crea una nueva nota

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,20 @@
-
+import { useDispatch } from "react-redux";
 import { IconButton } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../views";
 import { AddOutlined } from "@mui/icons-material";
+import { startNewNote } from "../../store/journal/thunks";
 
 
 export const JournalPage = () => {
+
+  const dispatch = useDispatch();
+
+  const onClickNewNote = () => {
+
+    dispatch( startNewNote() );
+  };
+
   return (
     <JournalLayout>
       {/* <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography> */}
@@ -13,6 +22,7 @@ export const JournalPage = () => {
       {/* <NoteView /> */}
 
       <IconButton
+       onClick={ onClickNewNote }
         size="large"
         sx={{
           color: "white",
