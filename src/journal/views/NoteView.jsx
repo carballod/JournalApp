13704060eb610692655f5c46a9bded33a942e790
Daniela Carballo <danscarballo@gmail.com>
2023-04-18 13:47:16 -0300

    Activar nota para su edicion

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,15 +1,28 @@
 import { SaveOutlined } from "@mui/icons-material"
 import { Button, Grid, TextField, Typography } from "@mui/material"
 import { ImageGallery } from "../components"
+import { useForm } from "../../hooks/useForm"
+import { useSelector } from "react-redux"
+import { useMemo } from "react"
 
 
 export const NoteView = () => {
+
+  const { active:note } = useSelector( state => state.journal );
+
+  const { body, title, date, onInputChange, formState } = useForm( note );
+
+  const dateString = useMemo( () => {
+    const newDate = new Date( date );
+    return newDate.toUTCString();
+  }, [date])
+
   return (
     <Grid container direction='row' justifyContent='space-between' alignItems='center' sx={{ mb: 1 }}
     className= 'animate__animated animate__fadeIn animate__faster'
     >
         <Grid item>
-            <Typography fontSize={ 39 } fontWeight='light' >Abril 2023</Typography>
+            <Typography fontSize={ 39 } fontWeight='light' >{ dateString }</Typography>
         </Grid>
         <Grid item>
             <Button color="primary" sx={{ padding: 2 }}>
@@ -26,6 +39,9 @@ export const NoteView = () => {
             placeholder="Ingrese un titulo"
             label="Titulo"
             sx={{ border: 'none', mb: 1 }}
+            name="title"
+            value={ title }
+            onChange={ onInputChange }
           />
 
           <TextField
@@ -35,6 +51,9 @@ export const NoteView = () => {
             multiline
             placeholder="¿Qué sucedió en el día de hoy?"
             minRows={ 5 }
+            name="body"
+            value={ body }
+            onChange={ onInputChange }
           />
         </Grid>
 
